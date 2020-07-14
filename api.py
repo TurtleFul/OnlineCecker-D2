fac0e19a8a9dfcfd01e7be68d97340a8ba72452a
turtleful <andrifann@gmail.com>
2020-07-15 00:25:38 +0200

    still testing

--- api.py
+++ api.py
@@ -11,4 +11,7 @@ r = requests.get("https://www.bungie.net/Platform/GlobalAlerts/", headers=HEADER
 #convert the json object we received into a Python dictionary object
 #and print the name of the item
 response = r.json()
-print(response['Message'])
+
+
+#print(response['Message'])
+print(response)
