620bae3eccc9c321b622c558cbf0049641f230e4
turtleful <andrifann@gmail.com>
2020-07-14 23:11:06 +0200

    no file please only objects

--- api.py
+++ api.py
@@ -8,16 +8,7 @@ HEADERS = {"X-API-Key":'57b427094a2e458abbd83c56d67bfa27'}
 #make request for uptime status
 r = requests.get("https://www.bungie.net/Platform/GlobalAlerts/", headers=HEADERS)
 
-#Write response data to file
-with open('response_data.json', 'w') as fd:
-    fd.write(r.text)
-
-
-log = open('response_data.json')
-for line in log:
-    print(line)
-
 #convert the json object we received into a Python dictionary object
 #and print the name of the item
 response = r.json()
-print(response)
+print(response['Message'])
