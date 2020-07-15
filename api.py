e0eda17715c4bda635c7411ceab0e4edbfcdfebd
turtleful <andrifann@gmail.com>
2020-07-15 12:55:36 +0200

    api key changed and no longer public...

--- api.py
+++ api.py
@@ -2,8 +2,12 @@
 
 import requests
 import json
+
+from vars import my_api_key
+
 #dictionary to hold extra headers
-HEADERS = {"X-API-Key":'57b427094a2e458abbd83c56d67bfa27'}
+HEADERS = {"X-API-Key":my_api_key}
+                        
 
 #make request for uptime status
 r = requests.get("https://www.bungie.net/Platform/GlobalAlerts/", headers=HEADERS)
@@ -14,4 +18,4 @@ response = r.json()
 
 
 #print(response['Message'])
-print(response)
+print(HEADERS)
